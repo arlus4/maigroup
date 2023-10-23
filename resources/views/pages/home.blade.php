5ff119e152b3b200d51d5eee7110be99983f10b2
romarioharyadi <romarioharyadi13@gmail.com>
2023-10-23 09:31:59 +0700

    push tgl (23-10-2023)
    - update tampilan home page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,239 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MaiTea - Indonesia</title>
+
+        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('assets/css/bootsnav.css') }}" >	
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+
+    </head>
+	
+	<body>
+        <section class="welcome-hero">
+            <div class="top-area">
+                <div class="header-area">
+                    <nav class="navbar navbar-default bootsnav  navbar-sticky navbar-scrollspy"  data-minus-value-desktop="70" data-minus-value-mobile="55" data-speed="1000">
+                        <div class="container">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+                                    <i class="fa fa-bars"></i>
+                                </button>
+                                <a class="navbar-brand" href="index.html">
+                                    <img src="{{ asset('assets/images/logo_mytea.png') }}" alt="">
+                                </a>
+                            </div>
+                            <div class="collapse navbar-collapse menu-ui-design" id="navbar-menu">
+                                <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
+                                    <li class=" scroll active">
+                                        <a href="#home">Beranda</a>
+                                    </li>
+                                    <li class="scroll">
+                                        <a href="#service">Tentang Kami</a>
+                                    </li>
+                                    <li class="scroll">
+                                        <a href="#featured-cars">Kontak Kami</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+
+            <div class="container">
+                <div class="welcome-hero-txt">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h2 class="css-dniud9a">Terobosan teh termurah dan terbaru yang ada di Indonesia</h2>
+                            <div class="row" style="margin-top: 20px;">
+                                <div class="col-md-6">
+                                    <button class="btn" style="width:100%;border-radius: 8px;background-color: #d4edda;border: 1.4px solid #c3e6cb;color: #155724;">
+                                        <span style="font-size: 17px;font-weight: 600;">Pesan Sekarang</span>
+                                    </button>
+                                </div>
+                                <div class="col-md-6">
+                                    <button class="btn" style="width:100%;border-radius:8px;background-color:#fff;border: 1.4px solid #d4edda;color: #155724;">
+                                        <span style="font-size: 17px;font-weight: 600;">Gabung Mitra MaiTea</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <img src="{{ asset('assets/images/bg_banner_2.png') }}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+				<div class="row">
+					<div class="col-md-12">
+                        <div class="model-search-content">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="single-model-search">
+                                        <h5 class="css-fhk2nsa">
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat eos inventore exercitationem molestiae nihil sapiente a voluptates laborum consequuntur iure incidunt mollitia, ad eligendi, nisi vel assumenda eum. Neque, sit!<br><br>
+                                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium fugiat cumque labore excepturi maxime a laborum cupiditate? Nisi unde expedita, est nobis vel, assumenda quas atque itaque ullam reiciendis nulla?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquid suscipit est dolorem molestias animi. Deleniti aspernatur consectetur odio recusandae obcaecati sequi unde facilis? Quos culpa voluptate temporibus possimus nemo!
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, enim? Et iusto nemo eaque dicta, ex vel tenetur laboriosam, voluptatibus reiciendis suscipit vitae tempore minima consequuntur expedita odio excepturi accusantium!<br><br>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam iusto asperiores quam, fugit ex, incidunt possimus fuga voluptas dolorem molestias blanditiis earum nobis atque. Corrupti eveniet odit eos quos adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit similique deleniti dolore, sunt fugiat cupiditate, aperiam possimus ea cumque ratione, sed atque et labore dicta! Facilis aliquam sed dicta! Voluptatem.
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="service" style="padding-bottom: 0px;">
+            <div class="css-dcnk23j">
+                <div class="css-fnk2skj">
+                    <div class="css-po2jsm">
+                        <div class="css-hjsa2js">
+                            <h3 class="css-cjdkja">Menu MaiTea Indonesia</h3>
+                        </div>
+                    </div>
+                    <div class="css-diop1ms">
+                        <div class="css-jk1hsk">
+                            <div class="css-lojd1h">
+                                <img class="css-menu-img" src="{{ asset('assets/images/teh_1.png') }}">
+                            </div>
+                            <div class="css-kanan-text">
+                                <h4>MaiTea Leci</h4>
+                            </div>
+                        </div>
+                        <div class="css-jk1hsk">
+                            <div class="css-lojd1h">
+                                <img class="css-menu-img" src="{{ asset('assets/images/teh_2.png') }}">
+                            </div>
+                            <div class="css-kanan-text">
+                                <h4>Taro</h4>
+                            </div>
+                        </div>
+                        <div class="css-jk1hsk">
+                            <div class="css-lojd1h">
+                                <img class="css-menu-img" src="{{ asset('assets/images/teh_3.png') }}">
+                            </div>
+                            <div class="css-kanan-text">
+                                <h4>MaiTea Lemon</h4>
+                            </div>
+                        </div>
+                        <div class="css-jk1hsk">
+                            <div class="css-lojd1h">
+                                <img class="css-menu-img" src="{{ asset('assets/images/teh_4.png') }}">
+                            </div>
+                            <div class="css-kanan-text">
+                                <h4>MaiTea Original</h4>
+                            </div>
+                        </div>
+                        <div class="css-jk1hsk">
+                            <div class="css-lojd1h">
+                                <img class="css-menu-img" src="{{ asset('assets/images/teh_5.png') }}">
+                            </div>
+                            <div class="css-kanan-text">
+                                <h4>Milo Original</h4>
+                            </div>
+                        </div>
+                        <div class="css-jk1hsk">
+                            <div class="css-lojd1h">
+                                <img class="css-menu-img" src="{{ asset('assets/images/teh_6.png') }}">
+                            </div>
+                            <div class="css-kanan-text">
+                                <h4>MaiTea Matcha</h4>
+                            </div>
+                        </div>
+                        <div class="css-jk1hsk">
+                            <div class="css-lojd1h">
+                                <img class="css-menu-img" src="{{ asset('assets/images/teh_7.png') }}">
+                            </div>
+                            <div class="css-kanan-text">
+                                <h4>Thai Tea</h4>
+                            </div>
+                        </div>
+                        <div class="css-jk1hsk">
+                            <div class="css-lojd1h">
+                                <img class="css-menu-img" src="{{ asset('assets/images/teh_8.png') }}">
+                            </div>
+                            <div class="css-kanan-text">
+                                <h4>Red Velvet</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="service" style="padding: 0px;">
+            <div class="css-dcnk23j">
+                <div class="row">
+                    <div class="col-md-6 css-scnk2n">
+                        <h4 class="css-cjdkja">Gabung Jadi Mitra MaiTea</h4>
+                        <p class="css-text-p">MaiTea Indonesia kini sudah memiliki lebih dari 1000 mitra yang tersebar diseluruh wilayah Indonesia dan masih akan bertambah lagi serta lebih dari 1 Juta Cup Terjual setiap bulan nya.</p>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="{{ asset('assets/images/bg_benefit.png') }}">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="testimoni" style="padding: 0px;">
+            <div class="css-cnk32ns">
+                <div class="css-dfnn2ka">
+                    <div class="css-tes-header" style="width: 100%;">
+                        <div>
+                            <h3 class="css-cjdkja" style="text-align: center!important;">Apa Kata Pengguna Mitra <span>MaiTea?</span></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="css-cnka2he">
+                            <div class="css-fjl2js">
+                                <img src="{{ asset('assets/images/c1.png') }}">
+                            </div>
+                            <h5>Om Brewok</h5>
+                            <h6>Pembisnis</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ea autem et exercitationem a tempore ipsa praesentium, voluptatem rerum assumenda perspiciatis.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="css-cnka2he">
+                            <div class="css-fjl2js">
+                                <img src="{{ asset('assets/images/c2.png') }}">
+                            </div>
+                            <h5>Malika</h5>
+                            <h6>Wirausaha</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ea autem et exercitationem a tempore ipsa praesentium, voluptatem rerum assumenda perspiciatis.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="css-cnka2he">
+                            <div class="css-fjl2js">
+                                <img src="{{ asset('assets/images/c3.png') }}">
+                            </div>
+                            <h5>Usup</h5>
+                            <h6>Wirausaha</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ea autem et exercitationem a tempore ipsa praesentium, voluptatem rerum assumenda perspiciatis.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="bag-faq" style="padding: 0px;">
+            <div class="css-cjdkja">FAQ (Frequently Asked Questions)</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quo obcaecati tempora ex eveniet incidunt debitis fugit, ipsum soluta deleniti eos, sit officiis odit voluptatem quos nemo. Aut, quisquam obcaecati.
+        </section>
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Check_Connection;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,11 @@ use App\Http\Controllers\Check_Connection;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'home']);
 
 Route::get('/check-connection', [Check_Connection::class, 'checkConnection']);
 Route::get('/check-php', [Check_Connection::class, 'checkPhp']);
