0a2d50e06bf352680ab700c6c553a024ef3425cb
Arfan Lusiandro <arfan955@gmail.com>
2023-10-25 03:09:41 +0700

    "Update API SP"

---
+++ app/Http/Controllers/API/Invoice_Controller.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
+
+class Invoice_Controller extends Controller
+{
+    public function sumPoint($pembeli_id, $outlet_id)
+    {
+        try {
+            DB::unprepared("EXEC maigroup.dbo.sum_point_user $pembeli_id, $outlet_id");
+            return response()->json(['message' => 'Prosedur berhasil dijalankan']);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function sumBonusUser(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'pembeli_id' => 'required|string|max:255',
+            'outlet_id' => 'required|string|max:255',
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+    
+        try {
+            // eksekusi raw SQL query
+            // Call the stored procedure
+            DB::unprepared("EXEC maigroup.dbo.sum_bonus_user '{$request->pembeli_id}', '{$request->outlet_id}'");
+    
+            // If the stored procedure does not return a result set,
+            // You can return a success message or any other appropriate response.
+            return response()->json(['message' => 'Prosedur berhasil dijalankan']);
+        } catch (\Exception $e) {
+            // Return an error if the query fails
+            return response()->json(['error' => 'Query Error: ' . $e->getMessage()], 500);
+        }
+    }        
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Auth_Controller;
+use App\Http\Controllers\API\Invoice_Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,13 @@ Route::post('login', [Auth_Controller::class, 'login']);
 Route::post('logout', [Auth_Controller::class, 'logout'])->middleware('auth:sanctum');
 Route::post('register', [Auth_Controller::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    Route::get('/check-session', [SessionController::class, 'check']);
-    return $request->user();
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     Route::post('/sum_bonus_user', [Invoice_Controller::class, 'sumBonusUser']);
+//     return $request->user();
+// });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/sum_bonus_user', [Invoice_Controller::class, 'sumBonusUser']);
+    Route::get('/sum_point/{pembeli_id}/{outlet_id}', [Invoice_Controller::class, 'sumPoint']);
 });
