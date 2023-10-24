8221c65554eb230fbc77b8bb906f7ba56abd5f89
Arfan Lusiandro <arfan955@gmail.com>
2023-10-24 16:14:03 +0700

    "Add database & Models"

---
+++ database/migrations/2023_10_24_041905_create_outlets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('outlets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('nama_outlet');
+            $table->string('slug')->unique();
+            $table->string('no_hp')->nullable();
+            $table->boolean('is_verified')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('outlets');
+    }
+};
