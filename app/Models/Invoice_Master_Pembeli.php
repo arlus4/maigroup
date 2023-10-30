94e48e90f921eb18ea6c1f314991e159d813b876
Arfan Lusiandro <arfan955@gmail.com>
2023-10-30 08:52:12 +0700

    Add Model Invoice

---
+++ app/Models/Invoice_Master_Pembeli.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice_Master_Pembeli extends Model
+{
+    use HasFactory;
+
+    protected $table = 'invoice_master_pembeli';
+
+    public $timestamps = false;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+}
