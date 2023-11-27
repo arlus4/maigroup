ec8986c94609d1e90a9382313b590ecd9841690c
Arfan Lusiandro <arfan955@gmail.com>
2023-11-27 16:08:25 +0700

    Update function get_data_rejected(), orderDetail(), downloadInvoice(), detailInvoicePenjualan(), downloadInvoice() and invoice.blade.php

--- app/Http/Controllers/Owner/ReportInvoiceController.php
+++ app/Http/Controllers/Owner/ReportInvoiceController.php
@@ -147,64 +147,19 @@ class ReportInvoiceController extends Controller
 
     public function downloadInvoice($invoice)
     {
-        $data = Invoice_Master_Seller::select
-        (
-            'invoice_master_seller.id as idInvoiceMasterSeller',
-            'invoice_master_seller.outlet_id',
-            'invoice_master_seller.invoice_no',
-            'invoice_master_seller.qty',
-            'invoice_master_seller.amount',
-            'invoice_master_seller.date_created',
-            'invoice_master_seller.ongkir',
-            'invoice_master_seller.total',
-            'invoice_master_seller.kode_unik',
-            
-            'outlets.id as idOutlets',
-            'outlets.user_id',
-            'outlets.nama_outlet',
-            
-            'users_details.nomor_telfon',
-            'users_details.kode_pos',
-            'users_details.alamat_detail',
-            
-            'ref_kelurahan.kode_kelurahan',
-            'ref_kelurahan.nama_kelurahan',
-            'ref_kecamatan.nama_kecamatan',
-            'ref_kotakab.nama_kotakab',
-            'ref_propinsi.nama_propinsi',
-        )
-        ->leftJoin('outlets','invoice_master_seller.outlet_id','=','outlets.outlet_id')
-        ->leftJoin('users_details','outlets.user_id','=','users_details.user_id')
-        ->leftJoin('ref_kelurahan','users_details.kelurahan','=','ref_kelurahan.kode_kelurahan')
-        ->leftJoin('ref_kecamatan','users_details.kecamatan','=','ref_kecamatan.kode_kecamatan')
-        ->leftJoin('ref_kotakab','users_details.kota_kabupaten','=','ref_kotakab.kode_kotakab')
-        ->leftJoin('ref_propinsi','users_details.provinsi','=','ref_propinsi.kode_propinsi')
-        ->where('invoice_master_seller.invoice_no', $invoice)
-        ->first();
+        $master = DB::select("SELECT 
+                invoice_no, nama_outlet, date_created_invoice, alamat_detail, nama_kelurahan, nama_kecamatan, nama_kotakab, nama_provinsi, kodepos, 
+                amount, ongkir, kode_unik, total
 
-        $details = Invoice_Detail_Seller::select(
-            'invoice_detail_seller.invoice_no',
-            'invoice_detail_seller.sku_id',
-            'invoice_detail_seller.qty as qtyDetailSeller',
-            'invoice_detail_seller.amount as amountDetailSeller',
-            'invoice_detail_seller.discount',
-            'invoice_detail_seller.total_amount',
-            'invoice_detail_seller.project_id',
-            
-            'ref_produks.sku',
-            'ref_produks.nama_produk',
-            'ref_produks.path_thumbnail',
+            FROM [maigroup].[dbo].[web.invoice_master_seller] ('". $invoice ."')");
+        $masterSeller = $master[0];
 
-            'ref_project.project_name',
-        )
-        ->leftJoin('ref_produks', 'invoice_detail_seller.sku_id', '=', 'ref_produks.sku')
-        ->leftJoin('ref_project', 'invoice_detail_seller.project_id', '=', 'ref_project.id')
-        ->where('invoice_detail_seller.invoice_no', $invoice)
-        ->get();
+        $detail = DB::select("SELECT sku_id, path_thumbnail, nama_produk, project_name, qty, total_amount
+                            FROM [maigroup].[dbo].[web.invoice_detail_seller] ('" . $invoice . "')");
 
         return view('owner.report-invoice.downloadInvoice', [
-            'data'    => $data,
-            'details' => $details
+            'data'    => $masterSeller,
+            'details' => $detail
         ]);
     }
 }
