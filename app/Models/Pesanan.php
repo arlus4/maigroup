9bb0d87c19c9c4baa3cd35bb85f37a218e8c458a
arlus4 <arfan955@gmail.com>
2024-07-19 16:08:29 +0700

    "Update Models"

--- app/Models/Pesanan.php
+++ app/Models/Pesanan.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Pesanan extends Model
 {
     use HasFactory;
+
+    protected $table = 'pesanan';
+    protected $primaryKey = 'pesanan_id'; // Nama kolom primary key
+    public $incrementing = false; // Non-incrementing atau bukan auto-increment
+    protected $keyType = 'string'; // Tipe data primary key adalah string
+
+    public $timestamps = false;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
 }
--- app/Models/Pesanan_Detail.php
+++ app/Models/Pesanan_Detail.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Pesanan_Detail extends Model
 {
     use HasFactory;
+
+    protected $table = 'pesanan_details';
+
+    public $timestamps = false;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
 }
