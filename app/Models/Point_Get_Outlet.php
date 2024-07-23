a326d88e91a2a458da51f6f001ebe9c4b514fda6
arlus4 <arfan955@gmail.com>
2024-07-23 15:34:53 +0700

    "Fix Bugs in Model Point_Get_Outlet"

--- app/Models/Point_Get_Outlet.php
+++ app/Models/Point_Get_Outlet.php
@@ -9,7 +9,7 @@ class Point_Get_Outlet extends Model
 {
     use HasFactory;
 
-    protected $table = 'point_deposit';
+    protected $table = 'point_get_outlet';
 
     /**
      * The attributes that aren't mass assignable.
