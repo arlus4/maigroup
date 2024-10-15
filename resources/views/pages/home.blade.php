96c18b1b34edf2ec33ffc2a4c906277b1cdc0526
arlus4 <arfan955@gmail.com>
2024-10-15 17:08:46 +0700

    "Update favicon to logo_ts.png"

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Toko Seru - Nusantara</title>
-        <link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
+        <link rel="icon" type="image/x-icon" href="assets/images/logo_ts.png">
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
